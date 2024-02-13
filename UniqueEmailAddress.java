587dfef30dcbbe207f6df21bc7085040decd7967
Lakshmi Avadhanula <51811327+lakshmi1403@users.noreply.github.com>
2024-02-12 21:56:27 -0800

    Add files via upload

---
+++ UniqueEmailAddress.java
@@ -0,0 +1,63 @@
+package com.example.myleetcodejourney;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class UniqueEmailAddress {
+
+    public static int numUniqueEmails(String[] emails) {
+        HashSet<String> set = new HashSet<>();
+        for(String s : emails){
+
+            StringBuilder sb = new StringBuilder();
+            int delimiter = s.indexOf("@");
+
+            for(int i=0;i<delimiter;i++){
+                char ch = s.charAt(i);
+                if(ch == '+') break;
+                if(ch!='.') sb.append(ch);
+            }
+
+            sb.append(s.substring(delimiter));
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+
+    public static void main(String[] args){
+
+            // Create a Scanner object for user input
+            Scanner scanner = new Scanner(System.in);
+
+            // Prompt the user to enter email addresses
+            System.out.println("Enter email addresses (type 'done' to finish input):");
+
+            // Create a HashSet to store unique email addresses
+            HashSet<String> emailSet = new HashSet<>();
+
+            // Keep taking user input until they type 'done'
+            while (true) {
+                String email = scanner.nextLine().trim();
+
+                // Check if the user wants to finish input
+                if (email.equalsIgnoreCase("done")) {
+                    break;
+                }
+
+                // Add the entered email to the HashSet
+                emailSet.add(email);
+            }
+
+            // Close the Scanner
+            scanner.close();
+
+            // Convert the HashSet to an array
+            String[] emails = emailSet.toArray(new String[0]);
+
+            // Call the numUniqueEmails method and print the result
+            int uniqueEmailsCount = numUniqueEmails(emails);
+            System.out.println("Number of unique emails: " + uniqueEmailsCount);
+        }
+}
+
+
