2d2a047ea4e48bca024ac28562b7d1664d2f4540
Lakshmi Avadhanula <51811327+lakshmi1403@users.noreply.github.com>
2024-02-15 12:21:56 -0800

    Add files via upload

---
+++ IntegerToRoman.java
@@ -0,0 +1,76 @@
+package com.example.myleetcodejourney;
+
+import java.util.Scanner;
+
+public class IntegerToRoman {
+        public static String intToRoman(int num) {
+            if (num < 1 || num > 3999)
+                return "Invalid Roman Number Value";
+
+            String s = "";
+            while (num >= 1000) {
+                s += "M";
+                num -= 1000;        }
+            while (num >= 900) {
+                s += "CM";
+                num -= 900;
+            }
+            while (num >= 500) {
+                s += "D";
+                num -= 500;
+            }
+            while (num >= 400) {
+                s += "CD";
+                num -= 400;
+            }
+            while (num >= 100) {
+                s += "C";
+                num -= 100;
+            }
+            while (num >= 90) {
+                s += "XC";
+                num -= 90;
+            }
+            while (num >= 50) {
+                s += "L";
+                num -= 50;
+            }
+            while (num >= 40) {
+                s += "XL";
+                num -= 40;
+            }
+            while (num >= 10) {
+                s += "X";
+                num -= 10;
+            }
+            while (num >= 9) {
+                s += "IX";
+                num -= 9;
+            }
+            while (num >= 5) {
+                s += "V";
+                num -= 5;
+            }
+            while (num >= 4) {
+                s += "IV";
+                num -= 4;
+            }
+            while (num >= 1) {
+                s += "I";
+                num -= 1;
+            }
+            return s;
+        }
+
+        public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter numeric number: ");
+            String i = scanner.nextLine();
+            int num=Integer.parseInt(i);
+
+            String romanNum= intToRoman(num);
+
+            System.out.println(romanNum);
+            scanner.close();
+        }
+}
